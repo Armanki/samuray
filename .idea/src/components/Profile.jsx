c9d24e7223086da6c595d3e512284cd5ac174161
Serob Kirakosyan <serob.kirakosyan.88@gmail.com>
2022-01-13 13:08:18 +0400

    initial commit

---
+++ .idea/src/components/Profile.jsx
@@ -0,0 +1,29 @@
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://media.istockphoto.com/photos/colorful-background-of-pastel-powder-explosionrainbow-color-dust-on-picture-id1180542165?k=20&m=1180542165&s=612x612&w=0&h=43hlhk8qdGYP4V-u3AAxD3kPDRIzHjMNWpr-VdBQ2Js="/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Profile
