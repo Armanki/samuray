4c2b6efe48cb30b627d6ea3b10bfa04fa8247014
Armanki <arman.kirakosyan.89@gmail.com>
2022-01-16 02:24:17 +0400

    Redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state from "./Redax/state";
+import state from "./Redux/state";
 
 ReactDOM.render(
     <React.StrictMode>
